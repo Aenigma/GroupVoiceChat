bbdbc7e8230880a3eecc58ebda7d3efe891bf8e0
Kevin Raoofi <nullindev@gmail.com>
2015-03-20 20:38:54 -0400

    Added Pair class.

---
+++ src/main/java/edu/frostburg/groupvoicechat/commons/Pair.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015 Kevin Raoofi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.frostburg.groupvoicechat.commons;
+
+/**
+ *
+ * @author Kevin Raoofi
+ * @param <L> left element type
+ * @param <R> right element type
+ */
+public class Pair<L, R> {
+
+    L left;
+    R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public Pair() {
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public void setLeft(L left) {
+        this.left = left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    public void setRight(R right) {
+        this.right = right;
+    }
+
+}
